7eca0f802b7973323a8a2596e4aec40896aa4915
Sgian24 <93100806+Sgian24@users.noreply.github.com>
2024-12-24 01:24:10 -0500

    Update Project section.

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -13,6 +13,10 @@ import hor2 from "..//Assets/hor-screen-2.jpg";
 import hor3 from "..//Assets/hor-screen-3.jpg";
 import hor4 from "..//Assets/hor-screen-4.jpg";
 import { Link } from "react-router-dom";
+import techno1 from "..//Assets/Techno-1.jpg";
+import techno2 from "..//Assets/Techno-2.jpg";
+import techno3 from "..//Assets/Techno-3.jpg";
+import techno4 from "..//Assets/Techno-4.jpg";
 
 const Home = () => {
 
@@ -53,7 +57,8 @@ const Home = () => {
             if (i[0].isIntersecting) {
                 document.getElementsByClassName("fade-in-row")[0].style.opacity = "1";
                 document.getElementsByClassName("fade-in-row")[1].style.opacity = "1";
-                document.getElementsByClassName("fade-in-row")[2].style.opacity = "1";
+                document.getElementsByClassName("fade-in-row")[2].style.opacity = "1"
+                document.getElementsByClassName("fade-in-row")[3].style.opacity = "1";
         }});
         const observerThree = new IntersectionObserver(i => {
             if (i[0].isIntersecting) {
@@ -81,7 +86,7 @@ const Home = () => {
                 <Col className="d-flex flex-column align-items-center justify-content-center pt-5 text-white">
                     <h5>Hi, my name is</h5>
                     <h1 className="name">SUNNY GIAN</h1>
-                    <h5>Frontend Developer based in Toronto.</h5>
+                    <h5>Front-End Developer based in Toronto.</h5>
                 </Col>
             </Row>
         </Container>
@@ -90,8 +95,8 @@ const Home = () => {
                 <Col className="mb-4 mb-md-4 mb-lg-0 mb-xl-0" md={12} lg={6} xl={6}>
                     <h1 className="heading">ABOUT</h1>
                     <hr />
-                    <p>My name is Sunny Gian. I am a frontend engineer with a UX design background. I have a passion for design but enjoy the more technical 
-                        aspects which lead me to get into frontend development. I am currently looking for opportunities to 
+                    <p>My name is Sunny Gian. I am a front-end engineer with a UX design background. I have a passion for design but enjoy the more technical 
+                        aspects which lead me to get into front-end development. I am currently looking for opportunities to 
                         further develop my abilities.</p>
                     <p><strong>Technical Skills:</strong></p> 
                     <ul>
@@ -120,18 +125,18 @@ const Home = () => {
             <Row className="fade-in-row mb-5">
                 <Col sm={12} xl={5} xxl={4} className="d-flex flex-column text-white">
                     <h4 className="mb-3">• NBA Fantasy Visualizer</h4>
-                    <p>A web app I developed to visualize NBA player stats with graphs. Users can create an account to draft and save their own roster. Data 
-                    is fetched from 3 different APIs. 
+                    <p>A web app I developed to present NBA player statistics in graphical form. Users can sign in to create and save their own roster. Data 
+                    is fetched from the "tank01-fantasy-stats" API (RapidAPI). 
                     </p>
-                    <p><strong>Frontend Development Tools Used:</strong></p> 
+                    <p><strong>Front-End Development Tools Used:</strong></p> 
                     <ul>
                        <li>React</li>
-                       <li>Various APIs from RapidAPI</li>
                        <li>Axios</li>
                        <li>Firebase Authentication and Firestore</li>
                        <li>Bootstrap</li>
                        <li>React Router</li>
                        <li>Recharts</li>
+                       <li>Formik and Yup Form Validation</li>
                     </ul>
                     <div className="pb-2 d-flex h-100 align-items-end">
                         <a className="btn btn-light me-3" target="_blank" href="https://sgian24.github.io/FantasyNBABasketball/">VIEW PROJECT</a>
@@ -149,11 +154,11 @@ const Home = () => {
                     </Carousel>
                 </Col>
             </Row>
-            <Row className="fade-in-row">
+            <Row className="fade-in-row mb-5">
                 <Col sm={12} xl={5} xxl={4} className="d-flex flex-column text-white">
                     <h4 className="mb-3">• Haus of Rogue Portfolio</h4>
                     <p>Responsive portfolio website I developed for graphic designer and artist Billie Rogue.</p>
-                    <p><strong>Frontend Development Tools Used:</strong></p> 
+                    <p><strong>Front-End Development Tools Used:</strong></p> 
                     <ul className="hor-portfolio-list">
                        <li>React</li>
                        <li>Bootstrap</li>
@@ -181,6 +186,45 @@ const Home = () => {
                     </Carousel>
                 </Col>
             </Row>
+            <Row className="fade-in-row">
+                <Col sm={12} xl={5} xxl={4} className="d-flex flex-column text-white">
+                    <h4 className="mb-3">• Toronto Techno Events</h4>
+                    <p>Web app I developed to browse upcoming techno events in Toronto. Users can 
+                       filter through events based on venues and/or dates. Google Maps integration
+                       also lets users browse events by location. Resource is fetched from a Firebase Firestore database
+                       that stores data curled from the "Real-Time Events Search" API.</p>
+                    <p><strong>Front-End Development Tools Used:</strong></p> 
+                    <ul className="hor-portfolio-list">
+                       <li>React</li>
+                       <li>Bootstrap</li>
+                       <li>React Router</li>
+                       <li>Google Maps API</li>
+                       <li>Firebase Firestore</li>
+                       <li>React Datepicker</li>
+                    </ul>
+                    <div className="pb-2 d-flex h-100 align-items-end">
+                        <a className="btn btn-light me-3" target="_blank" href="https://sgian24.github.io/TorontoTechnoEvents/">VIEW PROJECT</a>
+                        <a className="github-link text-white pb-2" href="https://github.com/Sgian24/TorontoTechnoEvents/" target="_blank">Github Repository</a>
+                    </div>
+                </Col>
+                <Col className="d-flex flex-column justify-content-top mt-5" sm={0} xl={7} xxl={8}>
+                    <Carousel className="carousel overflow-hidden" data-bs-theme="dark">
+                        <Carousel.Item >
+                            <img className="carousel" src={techno1} height="100%" width="100%" alt="" />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img className="carousel" src={techno2} width="100%" alt="" />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img className="carousel" src={techno3} width="100%" alt="" />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img className="carousel" src={techno4} width="100%" alt="" />
+                        </Carousel.Item>
+                    </Carousel>
+                </Col>
+            </Row>
+            
         </Container>
         <Container className="contact-container pt-5 pb-5 h-auto" ref={contactRef} fluid>
             <Row>
